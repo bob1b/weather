4f44f029c7e9ec90cbfd90e0eead535a0a86b62b
BobB <bailey.robert.a@gmail.com>
2017-08-14 11:53:25 -0400

    endpoint: fix line spacing

--- fetch.pl
+++ fetch.pl
@@ -33,34 +33,28 @@ sub fetch{
     return $response->content;
 }
 
-
 ####################
+
 # match zip code/city to location key
 # http://dataservice.accuweather.com/locations/v1/cities/search?apikey=Umn6NxjyQjHqjdYhUmvKQDVI1uNfHiXt&q=prescott%20valley&details=true&offset=1
 
 my $city_search_url = "$base/locations/v1/cities/search?apikey=$api_key&q=$query&details=true&offset=1";
 my $content = fetch($city_search_url);
-
 my $locations_ref = JSON->new->utf8->decode($content);
 
 ## use only the first location for now
 my $location = $locations_ref->[0];
 my $loc_id = $location->{'Details'}->{'Key'};
 
-
-
-####################
 # use location key to fetch current weather
 # http://dataservice.accuweather.com/currentconditions/v1/2134108?apikey=Umn6NxjyQjHqjdYhUmvKQDVI1uNfHiXt
 
 my $weather_url = "$base/currentconditions/v1/$loc_id?apikey=$api_key&details=true";
 $content = fetch($weather_url);
-
 my $weathers_ref = JSON->new->utf8->decode($content);
 
 # use only the first weather report for now
 my $weather = $weathers_ref->[0];
-
 
 # encode info hash as JSON for output
 my %out = ( 'location' => $location, 'weather' => $weather );
